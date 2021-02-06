c08c9d37b379ca04bf5de5b781cb401ded2a0ba3
Ty <tjgraves740@gmail.com>
2021-02-06 14:44:53 -0700

    Removed some code

--- script.py
+++ script.py
@@ -1,9 +1,4 @@
 import sys
 import requests
 
-print(sys.executable)
-print(sys.version)
-r = requests.get("https://www.google.com/")
-print(r.status_code)
-n = input()
-print(n * 2)
+print("hello world")
